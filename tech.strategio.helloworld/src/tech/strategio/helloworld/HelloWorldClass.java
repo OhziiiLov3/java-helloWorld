eb7f99b2ac7de0f7e2a4f6358db62533b5007ffb
ohziiilov3 <klbaskerville0520@gmail.com>
2022-01-14 16:07:26 -0800

    Java Data Types

--- tech.strategio.helloworld/src/tech/strategio/helloworld/HelloWorldClass.java
+++ tech.strategio.helloworld/src/tech/strategio/helloworld/HelloWorldClass.java
@@ -5,7 +5,18 @@ public class HelloWorldClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World");
+		String name = "Ohz";
+		System.out.print(name);
+		int myNum = 15;
+		System.out.print(myNum);
+		
 
 	}
+//	Java Variables 
+	
+	
+	
 
 }
+
+
