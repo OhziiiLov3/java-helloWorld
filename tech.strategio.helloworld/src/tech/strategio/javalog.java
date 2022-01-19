cd96f2ed0860faaf9845326c4c3d393b8c61121d
ohziiilov3 <klbaskerville0520@gmail.com>
2022-01-18 19:33:44 -0800

    added notes

--- tech.strategio.helloworld/src/tech/strategio/javalog.java
+++ tech.strategio.helloworld/src/tech/strategio/javalog.java
@@ -1,26 +1,26 @@
 package tech.strategio.helloworld;
 
 // ENCAPSULATION
-class Technologist {
+    class Technologist {
         String firstName;
         String lastName;
 //constructor
     Technologist() {
        firstName = "";
 }
-        void setFirstName (String n) {
+    void setFirstName(String n) {
         name = n;
     }
-        void getFirstName (String n) {
+    void getFirstName(String n) {
         return firstName;
     }
 }
 
 public class Test {
-    public static void main(String[]args){}
+    public static void main(String[] args){}
 //    Creating object DevOps of Tehcnologist
 //    Print name of DevOps
     Technologist devOps = new Technologist();
-    System.out.println("DevOps Technologlist First Name:" + devOps.getFirstName());
+    System.out.println("DevOps Technologist First Name:" + devOps.getFirstName());
 
 }
